b8cae6289eb54997851012b3eed56eb4a9960d2a
Colin Dean <colin.dean@metamesh.org>
2018-05-04 00:18:15 -0400

    Adds transfer normalization

--- bin/normalize-unifi.rb
+++ bin/normalize-unifi.rb
@@ -15,12 +15,24 @@ INPUT_FILE = ARGV.shift
 
 puts UnifiNormalizer::DurationNormalizer.normalize "1h 10m"
 
+
+def with_truthy_column row, column
+  position = ROW_DATA.index(column)
+  unnormalized = row[position]
+  if unnormalized
+    row[position] = yield(unnormalized)
+  end
+end
+
 output = CSV.generate do |csv|
   CSV.foreach INPUT_FILE do |row|
-    position = ROW_DATA.index(:duration)
-    unnormalized = row[position]
-    if unnormalized
-      row[position] = UnifiNormalizer::DurationNormalizer.normalize unnormalized
+    # position = ROW_DATA.index(:duration)
+    # unnormalized = row[position]
+    # if unnormalized
+    #   row[position] = UnifiNormalizer::DurationNormalizer.normalize unnormalized
+    # end
+    with_truthy_column row, :duration do |unnormalized|
+      UnifiNormalizer::DurationNormalizer.normalize unnormalized
     end
 
     #row[:rx] = TransferNormalizer::normalize_transfer row[:rx]
--- lib/unifi-export-normalizer/transfer_normalizer.rb
+++ lib/unifi-export-normalizer/transfer_normalizer.rb
@@ -1,7 +1,24 @@
 module UnifiNormalizer
   module TransferNormalizer
-    def self.normalize duration
+    def self.normalize transfer
+      match_data = /(\d*.?\d*) ([TGMK]?B)/.match transfer
+      quantity = Float(match_data[1])
+      units = match_data[2]
 
+      convert_to_megabytes(quantity, units).to_s
+    end
+
+    def self.convert_to_megabytes quantity, units
+      case units
+      when 'TB'
+        quantity * 1024.0 * 1024.0
+      when 'GB'
+        quantity * 1024.0
+      when 'MB'
+        quantity
+      when 'KB'
+        quantity / 1024.0
+      end
     end
   end
 end
